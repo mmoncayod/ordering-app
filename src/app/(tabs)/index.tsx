00b16f3af704d9cb4563eacc82b475a98f657336
Miguel-Raiil <106810869+Miguel-Raiil@users.noreply.github.com>
2024-05-10 18:26:41 +0200

    types definitions and alias paths

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,12 @@
 import { View } from "react-native";
-import products from "../../../assets/data/products";
-import ProductListItem from "../../components/ProductListItem";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
 
 export default function MenuScreen() {
   return (
     <View>
       <ProductListItem product={products[5]} />
       <ProductListItem product={products[1]} />
-      <ProductListItem product={products[2]} />
     </View>
   );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import Colors from "../constants/Colors";
+import { Product } from "@src/types";
 
-const ProductListItem = ({ product }) => {
+export const defaultPizza =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizza }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
