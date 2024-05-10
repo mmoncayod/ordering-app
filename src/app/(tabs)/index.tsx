b9191e11e1ccc1a94012e27bf43376883fc073f4
Miguel-Raiil <106810869+Miguel-Raiil@users.noreply.github.com>
2024-05-10 16:44:16 +0200

    testing

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from "@/src/components/Themed";
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Oneee</Text>
+      <Text style={styles.title}>Tab One</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
